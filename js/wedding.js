76aad8422a9c985fca24ed3ce08f5a7c7b68bd05
Marcela Kajanova <marcela.zubajova@gmail.com>
2018-01-21 15:22:10 +0100

    wedding quiz

---
+++ js/wedding.js
@@ -0,0 +1,34 @@
+"use strict";
+
+let quiz = ()=>
+{
+  let questions = [];
+  questions[0] = document.getElementsByName("q1");
+  questions[1] = document.getElementsByName("q2");
+  questions[2] = document.getElementsByName("q3");
+  questions[3] = document.getElementsByName("q4");
+  questions[4] = document.getElementsByName("q5");
+  questions[5] = document.getElementsByName("q6");
+  questions[6]= document.getElementsByName("q7");
+  questions[7] = document.getElementsByName("q8");
+  questions[8] = document.getElementsByName("q9");
+  questions[9] = document.getElementsByName("q10");
+  const ANSWERS = [1, 1, 2, 0, 0, 1, 1, 0, 3, 1];
+  let score = 0;
+  for (let i=0; i<10; i++)
+  {
+    if(questions[i][ANSWERS[i]].checked == true)
+    {
+      score++;
+    }
+  }
+  alert("vaše skóre je: " + score +"/10");
+}
+
+let btnService = ()=>
+{
+  let btn=document.getElementById("pushme");
+  btn.addEventListener("click", quiz, false);
+}
+
+window.addEventListener("load", btnService, false);
